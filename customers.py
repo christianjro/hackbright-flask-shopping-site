90585a33bf0b8c19bf8321d49c13fb6db447279b
Christian Rodriguez <crodriguezochoa@ucdavis.edu>
2023-03-16 16:34:52 -0700

    created Customer class and helper functions

--- customers.py
+++ customers.py
@@ -4,4 +4,30 @@
 class Customer:
     """Ubermelon customer."""
 
-    # TODO: need to implement this
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return f"<Customer: {self.first_name} {self.last_name}, {self.email}"
+
+
+def read_customers_from_file(filepath):
+    """Read customer data and populate dictionary of customers."""
+    
+    customers = {}
+
+    with open(filepath) as file:
+        for line in file:
+            first_name, last_name, email, password = line.strip().split("|")
+            customers[email] = Customer(first_name, last_name, email, password)
+
+    return customers
+
+def get_by_email(email):
+    """Return a customer, given their email."""
+    return customers[email]
+
+customers = read_customers_from_file("customers.txt")
--- shoppingsite.py
+++ shoppingsite.py
@@ -139,4 +139,4 @@ def checkout():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=3030)
