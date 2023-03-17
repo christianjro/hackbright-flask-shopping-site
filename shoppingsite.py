851a0a72fc7eab3ae3d1c6d11051c35037eacbcf
Christian Rodriguez <crodriguezochoa@ucdavis.edu>
2023-03-16 17:37:30 -0700

    logout route and handling

--- shoppingsite.py
+++ shoppingsite.py
@@ -111,16 +111,10 @@ def process_login():
     dictionary, look up the user, and store them in the session.
     """
 
-    # The logic here should be something like:
-    #
-    # - get user-provided name and password from request.form
     email = request.form.get("email")
     password = request.form.get("password")
-    # - use customers.get_by_email() to retrieve corresponding Customer
-    #   object (if any)
     customer = customers.get_by_email(email)
-    # - if a Customer with that email was found, check the provided password
-    #   against the stored one
+
     if customer is not None:
         if customer.password == password:
             session["logged_in_customer_email"] = customer.email
@@ -132,11 +126,14 @@ def process_login():
     else:
         flash("No customer with that email.")
         return redirect("/login")
-    # - if they match, store the user's email in the session, flash a success
-    #   message and redirect the user to the "/melons" route
-    # - if they don't, flash a failure message and redirect back to "/login"
-    # - do the same if a Customer with that email doesn't exist
 
+@app.route("/logout")
+def process_logout():
+    """Log user out."""
+    
+    del session["logged_in_customer_email"]
+    flash("You are logged out.")
+    return redirect("/melons")
 
 
 @app.route("/checkout")
